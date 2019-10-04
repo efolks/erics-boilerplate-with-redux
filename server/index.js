a9b1845956b7b300aabe34e8e7908c34f23e3fba
Eric Folks <efolks@live.com>
2019-10-04 13:39:10 -0400

    server set up and functional for port 3000, added start script, and hello div in index.html for testing

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const path = require('path')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
 
-app.use(morgan)
+app.use(morgan('combined'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -13,11 +15,15 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/api', require('./apiRoutes'))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+    res.sendFile(path.join(__dirname, '..', 'public'))
 })
 
 app.use((err, req, res, next) => {
     console.error(err)
     console.error(err.stack)
     res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on Port ${port}`)
 })
