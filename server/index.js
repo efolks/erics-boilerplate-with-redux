0e1300d4c94c8f7f928cc50f6371c44c4851a49d
Eric Folks <efolks@live.com>
2019-10-04 13:01:23 -0400

    sending index.html with every response

--- server/index.js
+++ server/index.js
@@ -11,3 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/api', require('./apiRoutes'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
