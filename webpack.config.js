98653455a0ec57ac73d578347245750e372c6fd6
Eric Folks <efolks@live.com>
2019-10-04 14:40:44 -0400

    devtool: source-maps aded to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
       path: __dirname, // assumes your bundle.js will also be in the root of your project folder
       filename: './public/bundle.js'
     },
-    //devtool: 'source-maps',
+    devtool: 'source-maps',
     module: {
       rules: [
         {
