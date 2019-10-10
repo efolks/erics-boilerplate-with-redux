3e7dee6a8daf311f2d84c0298f7f9f0e12be0518
Eric Folks <efolks@live.com>
2019-10-10 16:42:37 -0400

    intergrate session middleware and connected it to a sequlize sessions store

--- server/index.js
+++ server/index.js
@@ -5,13 +5,23 @@ const morgan = require('morgan')
 const path = require('path')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const sessionStore = new SequelizeStore({ db })
+const passport = require('passport')
 
 app.use(morgan('combined'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-
 app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'a wildly insecure secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+  }));
 
 app.use('/api', require('./apiRoutes'))
 
@@ -26,6 +36,7 @@ app.use((err, req, res, next) => {
 })
 
 async function startServer(){
+    await sessionStore.sync()
     await db.sync({force: true})
     app.listen(port, () => {
         console.log(`Server listening on Port ${port}`)
